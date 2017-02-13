7c0f6d5e11d647a52de3b4152fcf7c3471a151ca
micahscopes <fitchmicah@gmail.com>
2017-02-13 01:22:34 -0600

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+// Rollup plugins
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  entry: 'index.js',
+  acorn: {
+    allowReserved: true
+  },
+  dest: 'dist/multihop-forces.js',
+  format: 'iife',
+  // sourceMap: 'inline',
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      preferBuiltins: true  // Default: true
+    }),
+    commonjs(),
+    // eslint({
+    //   exclude: [
+    //     'src/styles/**',
+    //   ]
+    // }),
+    babel({
+      // exclude: [
+      //  'node_modules/**',
+      // '*.json'
+      // ],
+      // include: [
+      //   'node_modules/**'
+      // ],
+      presets: [
+        ['es2015', { modules: false }],
+          // 'stage-2'
+      ],
+      plugins: [
+        // 'transform-class-properties',
+        'external-helpers',
+        // 'transform-runtime'
+      ],
+      exclude: 'node_modules/babel-runtime/**',
+    //   externalHelpers: true,
+    }),
+  ],
+};
